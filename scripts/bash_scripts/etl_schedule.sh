239be4e677738d2abfc38eeb2fb0bef485a5181f
Patsy <Tzsapphire>
2025-09-10 16:58:20 +0200

    update etl sh

--- scripts/bash_scripts/etl_schedule.sh
+++ scripts/bash_scripts/etl_schedule.sh
@@ -14,7 +14,6 @@ curl "https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-e
 #changing the column_name
 sed -i 's/Variable_code/variable_code/g' full_path_filename
 
-
 # add folder path
 awk -F"," '{print $1, $5, $6, $9}' annual-enterprise-survey-2023-financial-year-provisional.csv > transformed/2023_year_finance.csv
 
