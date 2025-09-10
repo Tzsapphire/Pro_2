ce6be149b3930a46f4cac22b753b53ca9f457885
Patsy <Tzsapphire>
2025-09-10 14:02:25 +0200

    update script for moving csv-json files

--- scripts/bash_scripts/mov_files.sh
+++ scripts/bash_scripts/mov_files.sh
@@ -2,9 +2,12 @@
 
 #!/bin/bash
 
-#define the folder location (from folder : to folder)
-FROM_FOLDER=""
-TO_FOLDER=""
+#define the folder location (from folder & to folder)
+FROM_FOLDER="~/CDE/project_2/individual/Pro_2/bash_assi"
+TO_FOLDER="~/CDE/project_2/individual/Pro_2/scripts/movd_files"
 
 #move files to the new folder (could be wrong)
- cp "$(pwd)"/*.csv "$(pwd)"/*.json "$(pwd)/path_folder"
+ cp "$FROM_FOLDER"/*.csv "$FROM_FOLDER"/*.json "$TO_FOLDER"
+
+# cp "$(pwd)"/*.csv "$(pwd)"/*.json "$(pwd)/path_folder"
+
