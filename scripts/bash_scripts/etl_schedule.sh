9b9c2f8d34b7dcb22eed00e9f606e9708c3baac5
Patsy <Tzsapphire>
2025-09-10 21:18:00 +0200

    improve bash scripts

--- scripts/bash_scripts/etl_schedule.sh
+++ scripts/bash_scripts/etl_schedule.sh
@@ -5,23 +5,34 @@ mkdir raw
 mkdir transformed
 mkdir gold
 
+#define folder variables
+raw="RAW"
+transformed="TRANSFORMED"
+gold="GOLD"
+
 #link to url file
 URL="https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2023-financial-year-provisional/Download-data/annual-enterprise-survey-2023-financial-year-provisional.csv"
 
-#dowloading the file from the link
-curl "https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2023-financial-year-provisional/Download-data/annual-enterprise-survey-2023-financial-year-provisional.csv" -O #"file_#1.txt"
+#dowloading the file from the link, and save as the origin file name '-O'
+curl "$URL" -O 
+echo "saving file into $RAW folder"
 
-#changing the column_name
-sed -i 's/Variable_code/variable_code/g' full_path_filename
+#changing the column_name , '-i' to edit files in place and save to the raw directory
+sed -i 's/Variable_code/variable_code/g' "$(pwd)/$RAW"
 
-# add folder path
-awk -F"," '{print $1, $5, $6, $9}' annual-enterprise-survey-2023-financial-year-provisional.csv > transformed/2023_year_finance.csv
+# selecting specific table columns; storoing in the transformed folder and renaming the file
+awk -F"," '{print $1, $5, $6, $9}' annual-enterprise-survey-2023-financial-year-provisional.csv > $TRANSFORMED/2023_year_finance.csv
+echo "loaded into $TRANSFORMED folder"
 
 # copy folder to gold load the directory into gold
-# "cp transformed/2023_year_finance.csv gold/
 cp "$(pwd)/transformed/2023_year_finance.csv" "$(pwd)/gold/"
+echo "loaded into $GOLD folder"
 
 
-#schedule job to run daily
+#cron job to run daily
+CRON_FILE="~/CDE/project_2/individual/Pro_2/scripts/bash_scripts/etl_schedule.sh"
+
+# schedule job to run daily
 crontab -e
-0 00 * * * path_to_file
+0 00 * * * $CRON_FILE
+
