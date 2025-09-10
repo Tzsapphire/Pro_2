910fe99553bbe76d81f9812356d1dde8bb1f0c6a
Patsy <Tzsapphire>
2025-09-10 16:52:13 +0200

    test run moving files scripts

--- scripts/bash_scripts/mov_files.sh
+++ scripts/bash_scripts/mov_files.sh
@@ -3,8 +3,13 @@
 #!/bin/bash
 
 #define the folder location (from folder & to folder)
-FROM_FOLDER="~/CDE/project_2/individual/Pro_2/bash_assi"
-TO_FOLDER="~/CDE/project_2/individual/Pro_2/scripts/movd_files"
+#FROM_FOLDER="~/CDE/project_2/individual/Pro_2/bash_assi"
+#TO_FOLDER="~/CDE/project_2/individual/Pro_2/scripts/movd_files"
+
+FROM_FOLDER="./bash_assi"
+TO_FOLDER="./movd_files"
+
+mkdir -p "$TO_FOLDER"
 
 #move files to the new folder (could be wrong)
  cp "$FROM_FOLDER"/*.csv "$FROM_FOLDER"/*.json "$TO_FOLDER"
